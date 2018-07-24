8bd4a80e15203c34d4141ef074d95cb3549161dd
Anagha Dudihalli <Anagha.Dudihalli@betsol.com>
2018-07-25 04:24:44 +0530

    Custom filter

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -4,11 +4,13 @@ var myApp = angular
                 .module("myModule",[])
                 .controller("myController",function($scope){
                     var employees = [
-                        {   name: "Dolores", gender: "Female", salary: 60000, city: "London"},
-                        {    name: "Maeve", gender: "Female", salary: 65000, city: "New York"},
-                        {    name: "William",gender: "Male", salary: 49000, city: "Miami"},
-                        {    name: "Teddy", gender: "Male", salary: 55000, city: "Washington"},  
+                        {    name: "Dolores", gender: 2, salary: 60000, city: "London"},
+                        {    name: "Maeve", gender: 2, salary: 65000, city: "New York"},
+                        {    name: "William",gender: 1, salary: 49000, city: "Miami"},
+                        {    name: "Teddy", gender: 1, salary: 55000, city: "Washington"},  
+                        {    name: "Charlotte", gender: 3, salary: 35000, city: "Austin"}
                     ];
 
                    $scope.employees = employees;
+                   
                 });
