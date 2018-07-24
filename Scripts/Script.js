df5934051edf81efb07e3ec8a250b85495c528fa
Anagha Dudihalli <Anagha.Dudihalli@betsol.com>
2018-07-24 21:54:01 +0530

    Basic details of employees

---
+++ Scripts/Script.js
@@ -0,0 +1,14 @@
+/// <reference path="angular.js" />
+
+var myApp = angular
+                .module("myModule",[])
+                .controller("myController",function($scope){
+                    var employees = [
+                        {   name: "Dolores", gender: "Female", salary: 60000, city: "London"},
+                        {    name: "Maeve", gender: "Female", salary: 65000, city: "New York"},
+                        {    name: "William",gender: "Male", salary: 49000, city: "Miami"},
+                        {    name: "Teddy", gender: "Male", salary: 55000, city: "Washington"},  
+                    ];
+
+                   $scope.employees = employees;
+                 });
